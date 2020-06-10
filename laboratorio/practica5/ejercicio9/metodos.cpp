b2d60798d9a692a35d4791cb8504c099a773fcb1
Araozu <tbyuyfcjjll@gmail.com>
2020-06-10 18:28:22 -0500

    practica5/ejercicio9

---
+++ laboratorio/practica5/ejercicio9/metodos.cpp
@@ -0,0 +1,171 @@
+//
+// Created by Fernando on 06/06/2020.
+//
+#include "tipoDatos.h"
+
+int agregarAFichero(int numFichas, tipoDatos* fichas) {
+
+    if (numFichas >= 1000) {
+        std::cout << "Máximo de fichas alcanzado (1000)!" << std::endl;
+        return numFichas;
+    }
+
+    std::string nombre;
+    while (true) {
+        std::cout << "Introduce el nombre del fichero: ";
+        std::cin >> nombre;
+
+        if (nombre.empty()) {
+            std::cout << "Error. El nombre del fichero no puede estar vacio." << std::endl;
+        } else {
+            break;
+        }
+    }
+    fichas[numFichas].nombreFich = nombre;
+
+    int tamanyo;
+    while (true) {
+        std::cout << "Introduce el tamaño en KB: ";
+        std::cin >> tamanyo;
+
+        if (tamanyo < 0) {
+            std::cout << "Error. El tamaño del fichero no puede ser negativo." << std::endl;
+        } else {
+            break;
+        }
+    }
+    fichas[numFichas].tamanyo = tamanyo;
+
+    numFichas++;  // Y tenemos una ficha más
+
+    return numFichas;
+}
+
+void imprimirFicheros(int numFichas, tipoDatos* fichas) {
+    for (int i = 0; i < numFichas; i++) {
+        std::cout << "Nombre: " << fichas[i].nombreFich
+                  << "; Tamaño: " << fichas[i].tamanyo
+                  << "Kb" << std::endl;
+    }
+}
+
+void obtTamanoEImprFicherosMasGrandes(int numFichas, tipoDatos* fichas) {
+    int numeroTemporal;
+    std::cout << "¿A partir de que tamaño quieres que te muestre? ";
+    std::cin >> numeroTemporal;
+
+    for (int i = 0; i < numFichas; i++) {
+        if (fichas[i].tamanyo >= numeroTemporal) {
+            std::cout << "Nombre: " << fichas[i].nombreFich
+                      << "; Tamaño: " << fichas[i].tamanyo
+                      << " Kb" << std::endl;
+        }
+    }
+
+}
+
+void verDatosDeUnFichero(int numFichas, tipoDatos* fichas) {
+    std::string textoTemporal;
+    std::cout << "¿De qué fichero quieres ver todos los datos?";
+    std::cin >> textoTemporal;
+    for (int i = 0; i < numFichas; i++) {
+        if (fichas[i].nombreFich == textoTemporal) {
+            std::cout << "Nombre: " << fichas[i].nombreFich
+                      << "; Tamaño: " << fichas[i].tamanyo
+                      << " Kb" << std::endl;
+        }
+    }
+}
+
+void busquedaParcial(int numFichas, tipoDatos* fichas) {
+    std::string texto;
+    std::cout << "Ingresa una parte del nombre a buscar:";
+    std::cin >> texto;
+    for (int i = 0; i < numFichas; i++) {
+        if (fichas[i].nombreFich.find(texto) != std::string::npos) {
+            std::cout << "Nombre: " << fichas[i].nombreFich
+                      << "; Tamaño: " << fichas[i].tamanyo
+                      << " Kb" << std::endl;
+        }
+    }
+}
+
+int eliminarFichero(int numFichas, tipoDatos* fichas) {
+    int posicion;
+    while (true) {
+        std::cout << "Ingresa la posición a borrar:";
+        std::cin >> posicion;
+
+        if (posicion < 0 || posicion >= numFichas) {
+            std::cout << "La posición indicada es invalida.";
+            return numFichas;
+        } else {
+            break;
+        }
+    }
+
+    for (int i = posicion; i < numFichas - 1; i++) {
+        fichas[i] = fichas[i + 1];
+    }
+
+    return numFichas - 1;
+}
+
+void modificarFichero(int numFichas, tipoDatos* fichas) {
+    int posicion;
+    while (true) {
+        std::cout << "Ingresa la posición a modificar:";
+        std::cin >> posicion;
+
+        if (posicion < 0 || posicion >= numFichas) {
+            std::cout << "La posición indicada es invalida.";
+        } else {
+            break;
+        }
+    }
+
+    std::string nuevoNombre;
+    std::cout << "Ingresa el nuevo nombre (vacio para omitir):";
+    std::cin.ignore();
+    getline(std::cin, nuevoNombre);
+    if (!nuevoNombre.empty()) {
+        fichas[posicion].nombreFich = nuevoNombre;
+    }
+
+
+    std::string nuevoTamanio;
+    std::cout << "Ingresa el nuevo tamaño (vacio para omitir):";
+    std::cin.ignore();
+    getline(std::cin, nuevoTamanio);
+    if (!nuevoTamanio.empty()) {
+        fichas[posicion].tamanyo = std::stoi(nuevoTamanio);
+    }
+
+}
+
+void ordenarFicheros(int numFichas, tipoDatos* ficheros) {
+
+    tipoDatos ficheroAuxiliar;
+    for (int i = 1; i < numFichas; i++) {
+        tipoDatos ficheroAMover = ficheros[i];
+
+        for (int j = i - 1; j >= 0; j--) {
+            tipoDatos ficheroAComparar = ficheros[j];
+            if (ficheroAMover.nombreFich.compare(ficheroAComparar.nombreFich) < 0) {
+                ficheroAuxiliar.nombreFich = ficheroAMover.nombreFich;
+                ficheroAuxiliar.tamanyo = ficheroAMover.tamanyo;
+
+                ficheros[i].nombreFich = ficheroAComparar.nombreFich;
+                ficheros[i].tamanyo = ficheroAComparar.tamanyo;
+
+                ficheros[j].nombreFich = ficheroAuxiliar.nombreFich;
+                ficheros[j].tamanyo = ficheroAuxiliar.tamanyo;
+            } else {
+                break;
+            }
+        }
+
+    }
+
+}
+
---
+++ laboratorio/practica5/ejercicio9/metodos.h
@@ -0,0 +1,18 @@
+//
+// Created by Fernando on 06/06/2020.
+//
+
+#ifndef EJERCICIO1_METODOS_H
+#define EJERCICIO1_METODOS_H
+#include "tipoDatos.h"
+
+int agregarAFichero(int numFichas, tipoDatos* fichas);
+void imprimirFicheros(int numFichas, tipoDatos* fichas);
+void obtTamanoEImprFicherosMasGrandes(int numFichas, tipoDatos* fichas);
+void verDatosDeUnFichero(int numFichas, tipoDatos* fichas);
+void busquedaParcial(int numFichas, tipoDatos* fichas);
+int eliminarFichero(int numFichas, tipoDatos* fichas);
+void modificarFichero(int numFichas, tipoDatos* fichas);
+void ordenarFicheros(int numFichas, tipoDatos* ficheros);
+
+#endif //EJERCICIO1_METODOS_H
